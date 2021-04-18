ba50f6a95348650f4223479c92fe07a82d08ce24
Ekky Sukmawati <>
2021-04-18 11:51:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#Write your code below this row 👇
+
+total = 0
+for number in range(2, 101):
+  if number % 2 == 0:
+    total += number
+print(total)
+
+jumlah = 0
+for nomor in range(2, 101, 2):
+  jumlah += nomor
+print(jumlah)
